0309a45a76545980b311449714b6c8d0c6e8820c
Lorenzo <lorenzogatta.lg@gmail.com>
2025-03-30 19:35:46 +0200

    Initial Commit

---
+++ MyRunCoach/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyRunCoach
+//
+//  Created by Lorenzo Gatta on 30/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
